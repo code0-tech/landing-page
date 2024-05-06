b344c350ea9ea7d97cc76a62a33f104f527074f1
nicosammito <nico@sammito.de>
2024-05-06 21:21:31 +0200

    removing logo prop from quotes

--- src/static-components/CommunitySection.tsx
+++ src/static-components/CommunitySection.tsx
@@ -13,15 +13,15 @@ export const CommunitySection: React.FC = () => {
                 </Quote>
             </Col>
             <Col xs={12} lg={3} mb={1}>
-                <Quote name={"Dario Pranjic"} position={"Co-Founder Code0"} logo={"sds"}>
-                    Code0 is a game-changer, simplifying the creative process and letting me focus on the big picture without any coding needed
+                <Quote name={"Dario Pranjic"} position={"Ruby / Java backend developer"}>
+                    Code0 is a game-changer, simplifying the creative process and letting me focus on the big picture
+                    without any coding needed
                 </Quote>
             </Col>
             <Col xs={12} lg={3} mb={1}>
-                <Quote name={"Niklas van Schrick"} position={"Gitlab Core-Team"}
-                       logo={""}>
-                    The product enables us to create complex backend infrastructure in no-time.
-                    It completely replaces the need to lean any programming language and everyone can build flows.
+                <Quote name={"Niklas van Schrick"} position={"Gitlab Core-Team"}>
+                    Code0 allows me to quickly create my personal projects and host it without complex infrastructure
+                    configuration or server maintenance.
                 </Quote>
             </Col>
         </Row>
